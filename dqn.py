9cd45c0a993b228efcbcfc7a651570ece080b24a
klo9klo9kloi <jszou@berkeley.edu>
2020-04-19 16:53:20 -0700

    pytorch implementation

---
+++ dqn.py
@@ -0,0 +1,159 @@
+import uuid
+import time
+import pickle
+import os
+import sys
+import gym.spaces
+import itertools
+import numpy as np
+import random
+import logz
+import torch
+import torch.nn as nn
+import torchvision
+from collections import namedtuple
+from dqn_utils import *
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def huber_loss(x, delta=1.0):
+    """https://en.wikipedia.org/wiki/Huber_loss
+    """
+    return torch.where(
+        torch.abs(x) < delta,
+        torch.pow(x, 2) * 0.5,
+        delta * (torch.abs(x) - 0.5 * delta)
+    )
+
+
+def step_env(env, replay_buffer, num_actions, exploration_schedule, t, last_obs, model_initialized, policy):
+    frame_idx = replay_buffer.store_frame(last_obs)
+    encoded_obs = replay_buffer.encode_recent_observation()
+    explor_prob = exploration_schedule.value(t)
+
+    if not model_initialized or np.random.random() < explor_prob:
+        action = np.random.randint(num_actions)
+    else:
+        with torch.no_grad():
+            policy.eval()
+            encoded_obs = torchvision.transforms.functional.to_tensor(encoded_obs)
+            encoded_obs = encoded_obs.reshape([1] + list(encoded_obs.shape)).to(device)
+            q_vals = policy(encoded_obs)
+        action = np.argmax(q_vals.cpu().numpy())
+
+    obs, reward, done, info = env.step(action)
+    replay_buffer.store_effect(frame_idx, action, reward, done)
+    if done:
+        obs = env.reset()
+    return obs
+
+def update_model(optimizer, t, replay_buffer, policy, target, gamma, clip, batch_size, num_actions,
+                    learning_starts, learning_freq, num_param_updates, target_update_freq, double_q):
+    if (t > learning_starts and \
+        t % learning_freq == 0 and \
+        replay_buffer.can_sample(batch_size)):
+
+        obs_batch, act_batch, rew_batch, next_obs_batch, done_mask = replay_buffer.sample(batch_size)
+        obs_batch = torch.stack([torchvision.transforms.functional.to_tensor(ob) for ob in obs_batch]).to(device)
+        next_obs_batch = torch.stack([torchvision.transforms.functional.to_tensor(ob) for ob in next_obs_batch]).to(device)
+        act_batch = torch.from_numpy(act_batch).type(torch.LongTensor).to(device)
+        rew_batch = torch.from_numpy(rew_batch).to(device)
+        done_mask = torch.from_numpy(done_mask).to(device)
+
+        with torch.no_grad():
+            q_target_tp1 = target(next_obs_batch)
+
+        policy.train()
+        optimizer.zero_grad()
+        policy.zero_grad()
+
+        q_t = policy(obs_batch).gather(1, act_batch.reshape(-1, 1)).flatten()
+
+        if double_q:
+            with torch.no_grad():
+                policy.eval()
+                q_tp1 = torch.argmax(policy(next_obs_batch), axis=1)
+                y_t = rew_batch + (1-done_mask)*gamma*q_target_tp1.gather(1, q_tp1.reshape(-1, 1)).flatten()
+        else:
+            with torch.no_grad():
+                y_t = rew_batch + (1-done_mask)*gamma*torch.max(q_target_tp1, axis=1)[0].reshape(-1, 1).flatten()
+
+        total_error = torch.mean(huber_loss(y_t - q_t))
+        total_error.backward()
+        nn.utils.clip_grad_norm_(policy.parameters(), clip)
+        optimizer.step()
+
+        if num_param_updates % target_update_freq == 0:
+            target.load_state_dict(policy.state_dict())
+
+
+def log_progress(env, t, log_every_n_steps, lr, start_time, exploration, best_mean_episode_reward):
+    episode_rewards = get_wrapper_by_name(env, "Monitor").get_episode_rewards()
+
+    if len(episode_rewards) > 0:
+        mean_episode_reward = np.mean(episode_rewards[-100:])
+        std_episode_reward  = np.std(episode_rewards[-100:])
+    if len(episode_rewards) > 100:
+        best_mean_episode_reward = \
+            max(best_mean_episode_reward, mean_episode_reward)
+
+    # See the `log.txt` file for where these statistics are stored.
+    if t % log_every_n_steps == 0:
+        hours = (time.time() - start_time) / (60.*60.)
+        logz.log_tabular("Steps",                 t)
+        logz.log_tabular("Avg_Last_100_Episodes", mean_episode_reward)
+        logz.log_tabular("Std_Last_100_Episodes", std_episode_reward)
+        logz.log_tabular("Best_Avg_100_Episodes", best_mean_episode_reward)
+        logz.log_tabular("Num_Episodes",          len(episode_rewards))
+        logz.log_tabular("Exploration_Epsilon",   exploration.value(t))
+        logz.log_tabular("Adam_Learning_Rate",    lr)
+        logz.log_tabular("Elapsed_Time_Hours",    hours)
+        logz.dump_tabular()
+    return best_mean_episode_reward
+
+
+def learn(env,
+         q_func_model,
+         exploration,
+         replay_buffer_size,
+         batch_size,
+         gamma,
+         learning_starts,
+         learning_freq,
+         frame_history_len,
+         target_update_freq,
+         grad_norm_clipping,
+         double_q=True,
+         logdir=None,
+         max_steps=2e8):
+
+    num_actions = env.action_space.n
+    policy = q_func_model(3 * frame_history_len, num_actions).to(device)
+    target = q_func_model(3 * frame_history_len, num_actions).to(device)
+    target.load_state_dict(policy.state_dict())
+    target.eval()
+
+    optimizer = torch.optim.Adam(policy.parameters(), lr=1e-4)
+    replay_buffer = ReplayBuffer(replay_buffer_size, frame_history_len)
+    replay_buffer_idx = None
+
+    start_time = time.time()
+    log_every_n_steps = 10000
+
+    t = 0
+    num_param_updates = 0
+    best_mean_episode_reward = float('-inf')
+
+    last_obs = env.reset()
+
+    while True:
+        last_obs = step_env(env, replay_buffer, num_actions, exploration, t, last_obs, t > learning_starts, policy)
+
+        update_model(optimizer, t, replay_buffer, policy, target, gamma, grad_norm_clipping, batch_size, num_actions,
+                    learning_starts, learning_freq, num_param_updates, target_update_freq, double_q)
+        num_param_updates += 1
+        t += 1
+        log_progress(env, t, log_every_n_steps, optimizer.param_groups[0]['lr'], start_time, exploration, best_mean_episode_reward)
+        if t > max_steps:
+            print("\nt = {} exceeds max_steps = {}".format(t, max_steps))
+            sys.exit()
---
+++ train_dqn.py
@@ -0,0 +1,99 @@
+import os, random, time, argparse, gym, sys
+import logz
+from gym import wrappers
+import numpy as np
+import torch
+import torchvision
+import torch.nn as nn
+import dqn
+from dqn_utils import *
+# from procgen import ProcgenEnv
+
+class base_atari_model(nn.Module):
+    def __init__(self, input_channels, action_dim):
+        super(base_atari_model, self).__init__()
+        self.convnet = nn.Sequential(
+            nn.Conv2d(input_channels, 32, 8, stride=4),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, stride=1),
+            nn.ReLU(inplace=True)
+        )
+        self.action_value = nn.Sequential(
+            nn.Linear(64*4*4, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, action_dim)
+        )
+    def forward(self, x):
+        x = self.convnet(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.action_value(x)
+        return x
+
+def learn(env, args):
+    # lr_schedule = ConstantSchedule(1e-4)
+    limit = max(int(args.num_steps/2), 2e6)
+    exploration_schedule = PiecewiseSchedule([
+            (0,     1.00),
+            (1e6,   0.10),
+            (limit, 0.01),
+        ], outside_value=0.01
+    )
+    dqn.learn(
+        env=env,
+        q_func_model=base_atari_model,
+        exploration=exploration_schedule,
+        replay_buffer_size=1000000,
+        batch_size=32,
+        gamma=0.99,
+        learning_starts=50000,
+        learning_freq=4,
+        frame_history_len=4,
+        target_update_freq=10000,
+        grad_norm_clipping=10,
+        double_q=args.double_q,
+        logdir=args.logdir,
+        max_steps=args.num_steps
+    )
+    env.close()
+
+
+def set_global_seeds(i):
+    torch.manual_seed(i)
+    np.random.seed(i)
+    random.seed(i)
+
+def get_env(args):
+    env = gym.make("procgen:procgen-" + args.env + "-v0", num_levels=args.num_levels, start_level=args.start_seed, distribution_mode='easy')
+    set_global_seeds(args.seed)
+    expt_dir = os.path.join(args.logdir, "procgen")
+    return wrappers.Monitor(env, expt_dir, force=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('env', type=str)
+    parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--start_seed', type=int, default=0)
+    parser.add_argument('--num_steps', type=int, default=1e5) #100K, 250K, or 1M
+    parser.add_argument('--double_q', action='store_true', default=False)
+    parser.add_argument('--num_levels', type=int, default=50) #50, 100, 250, 500
+    # parser.add_argument('--num_envs', type=int, default=1) # can be used for parallel agents?
+    args = parser.parse_args()
+
+    assert args.env in ['coinrun', 'caveflyer', 'jumper']
+    if args.seed is None:
+        args.seed = random.randint(0, 9999)
+    print('random seed = {}'.format(args.seed))
+    exp_name = 'dqn'
+    if args.double_q:
+        exp_name = 'double-dqn'
+
+    if not(os.path.exists('data_dqn')):
+        os.makedirs('data_dqn')
+    logdir = exp_name+ '_' +args.env+ '_' +time.strftime("%d-%m-%Y_%H-%M-%S")
+    logdir = os.path.join('data_dqn', logdir)
+    logz.configure_output_dir(logdir)
+    args.logdir = logdir
+    env = get_env(args)
+    learn(env, args)
