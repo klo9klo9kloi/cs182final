432be06a57a04d21b04d0de602f8eb43610362e5
klo9klo9kloi <jszou@berkeley.edu>
2020-04-26 16:10:52 -0700

    minor fixes

--- train_dqn_tf.py
+++ train_dqn_tf.py
@@ -66,7 +66,7 @@ def learn(env, session, args):
             replay_buffer_size=1000000,
             batch_size=32,
             gamma=0.99,
-            learning_starts=10000,
+            learning_starts=50000,
             learning_freq=4,
             frame_history_len=4,
             target_update_freq=10000,
