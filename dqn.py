2802f51d71ec78804811759c782cc5a252d6aed9
Jinwoo Park <jinwoopark1673@gmail.com>
2020-04-27 09:24:15 -0700

    h and n-step test

--- dqn.py
+++ dqn.py
@@ -26,29 +26,56 @@ def huber_loss(x, delta=1.0):
     )
 
 
-def step_env(env, replay_buffer, num_actions, exploration_schedule, t, last_obs, model_initialized, policy):
+def step_env(env, replay_buffer, num_actions, exploration_schedule, t, last_obs, model_initialized, policy, n, gamma):
     frame_idx = replay_buffer.store_frame(last_obs)
     encoded_obs = replay_buffer.encode_recent_observation()
     explor_prob = exploration_schedule.value(t)
 
-    if not model_initialized or np.random.random() < explor_prob:
-        action = np.random.randint(num_actions)
-    else:
-        with torch.no_grad():
-            policy.eval()
-            encoded_obs = torchvision.transforms.functional.to_tensor(encoded_obs)
-            encoded_obs = encoded_obs.reshape([1] + list(encoded_obs.shape)).to(device)
-            q_vals = policy(encoded_obs)
-        action = np.argmax(q_vals.cpu().numpy())
+    first_action = -1
+    first_done = False
+    first_obs = None
+    reward = 0
 
-    obs, reward, done, info = env.step(action)
-    replay_buffer.store_effect(frame_idx, action, reward, done)
-    if done:
-        obs = env.reset()
-    return obs
+    for i in range(n):
+        if not model_initialized or np.random.random() < explor_prob:
+            action = np.random.randint(num_actions)
+        else:
+            with torch.no_grad():
+                policy.eval()
+                encoded_obs = torchvision.transforms.functional.to_tensor(encoded_obs)
+                encoded_obs = encoded_obs.reshape([1] + list(encoded_obs.shape)).to(device)
+                q_vals = policy(encoded_obs)
+            action = np.argmax(q_vals.cpu().numpy())
+
+        if i == 0:
+            first_action = action
+
+        encoded_obs, rew, done, info = env.step(action)
+        if i == 0:
+            first_obs = encoded_obs
+            first_done = done
+
+        reward += rew * pow(gamma, i)
+
+        if done:
+            replay_buffer.store_effect(frame_idx, first_action, reward, first_done)
+            if i == 0:
+                first_obs = env.reset()
+            else:
+                env.reset()
+            return first_obs
+
+    replay_buffer.store_effect(frame_idx, first_action, reward, first_done)
+    return first_obs
+
+def hf(x, epsilon = 0.0001):
+    return torch.sign(x) * (torch.sqrt(torch.abs(x) + 1) - 1) + epsilon * x
+
+def h_inv(x, epsilon = 0.0001):
+    return torch.sign(x) * (1 + torch.abs(x - torch.sign(x) * x * epsilon)) ** 2 - 1
 
 def update_model(optimizer, t, replay_buffer, policy, target, gamma, clip, batch_size, num_actions,
-                    learning_starts, learning_freq, num_param_updates, target_update_freq, double_q, pr, beta):
+                    learning_starts, learning_freq, num_param_updates, target_update_freq, double_q, pr, beta, h):
     if (t > learning_starts and \
         t % learning_freq == 0 and \
         replay_buffer.can_sample(batch_size)):
@@ -74,15 +101,21 @@ def update_model(optimizer, t, replay_buffer, policy, target, gamma, clip, batch
         policy.zero_grad()
 
         q_t = policy(obs_batch).gather(1, act_batch.reshape(-1, 1)).flatten()
-        
+
         if double_q:
             with torch.no_grad():
                 policy.eval()
                 q_tp1 = torch.argmax(policy(next_obs_batch), axis=1)
-                y_t = rew_batch + (1-done_mask)*gamma*q_target_tp1.gather(1, q_tp1.reshape(-1, 1)).flatten()
+                if h:
+                    y_t = hf(rew_batch + (1-done_mask) * gamma * h_inv(q_target_tp1.gather(1, q_tp1.reshape(-1, 1)).flatten()))
+                else:
+                    y_t = rew_batch + (1-done_mask)*gamma*q_target_tp1.gather(1, q_tp1.reshape(-1, 1)).flatten()
         else:
             with torch.no_grad():
-                y_t = rew_batch + (1-done_mask)*gamma*torch.max(q_target_tp1, axis=1)[0].reshape(-1, 1).flatten()
+                if h:
+                    y_t = hf(rew_batch + (1-done_mask) * gamma * h_inv(torch.max(q_target_tp1, axis=1)[0].reshape(-1, 1).flatten()))
+                else:
+                    y_t = rew_batch + (1-done_mask)*gamma*torch.max(q_target_tp1, axis=1)[0].reshape(-1, 1).flatten()
 
         if pr:
             is_weights = (priorities*replay_buffer.size + 1e-10)**-beta
@@ -146,7 +179,9 @@ def learn(env,
          double_q=True,
          pr=False,
          beta=0.4,
-         alpha=0.6):
+         alpha=0.6,
+         n=1,
+         h=False):
 
     num_actions = env.action_space.n
     policy = q_func_model(3 * frame_history_len, num_actions).to(device)
@@ -171,10 +206,10 @@ def learn(env,
     last_obs = env.reset()
 
     while True:
-        last_obs = step_env(env, replay_buffer, num_actions, exploration, t, last_obs, t > learning_starts, policy)
+        last_obs = step_env(env, replay_buffer, num_actions, exploration, t, last_obs, t > learning_starts, policy, n, gamma)
 
         num_param_updates = update_model(optimizer, t, replay_buffer, policy, target, gamma, grad_norm_clipping, batch_size, num_actions,
-                    learning_starts, learning_freq, num_param_updates, target_update_freq, double_q, pr, beta)
+                    learning_starts, learning_freq, num_param_updates, target_update_freq, double_q, pr, beta, h)
         t += 1
         log_progress(env, t, log_every_n_steps, optimizer.param_groups[0]['lr'], start_time, exploration, best_mean_episode_reward)
         if t > max_steps:
